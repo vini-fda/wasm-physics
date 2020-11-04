ef0de908e55710e46b4287c85986a0606cca8fbd
Vinicius Freitas de Almeida <vinifreitas.d.a@gmail.com>
2020-11-04 14:03:24 -0300

    Added glm library and turned vertices into a static member variable

--- cpp/src/Atom.h
+++ cpp/src/Atom.h
@@ -27,14 +27,21 @@ public:
     //void processInput();
     //Updates physics of the Atom
     void update(float dt);
-    /* TODO: integrate C++ and javascript */
+    friend float collision_time(const Atom& a, const Atom& b);
+
+    ~Atom() {
+        count_instances--;
+    }
 
 private:
     std::unique_ptr<Shader> shader;
-    static const int NUM_VERTICES = 32;
-    float vertices[3*NUM_VERTICES];
+    static constexpr int NUM_VERTICES = 32;
+    static float vertices[3*NUM_VERTICES];
+    static int count_instances;
     unsigned int VAO=0;
     unsigned int VBO=0;
+
+    glm::mat4 model;
 };
 
 
