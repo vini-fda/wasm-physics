17bd2b8261e70fb2746e06340e572cc84679a804
Vinicius Freitas de Almeida <vinifreitas.d.a@gmail.com>
2020-11-04 12:47:39 -0300

    Major improvements to class' OpenGL integration (mostly rendering)

--- cpp/src/Atom.cpp
+++ cpp/src/Atom.cpp
@@ -3,6 +3,63 @@
 //
 
 #include "Atom.h"
+#include <GL/glew.h>
+#include <cmath>
+#include <memory>
+
+//#include <glm/gtc/matrix_transform.hpp>
+//using namespace glm;
+#include <string>
+
+Atom::Atom()
+{
+    //Vertices set up
+    for (size_t i = 0; i < NUM_VERTICES; i++) {
+        float theta = 2.0*i*M_PI/NUM_VERTICES;
+        vertices[3*i] = cosf(theta);
+        vertices[3*i+1] = sinf(theta);
+        vertices[3*i+2] = 0.0;
+    }
+    //Vertex shader
+    std::string vs = "#version 100\n"
+                     "attribute vec4 vPos;\n"
+                     "void main()\n"
+                     "{\n"
+                     "   gl_Position = vPos;\n"
+                     "}";
+    //Fragment shader
+    std::string fs = "#version 100\n"
+                     "precision mediump float;\n"
+                     "void main()\n"
+                     "{\n"
+                     "   gl_FragColor = vec4(1.0, 0.0, 0.2, 1.0);\n"
+                     "}";
+
+    //Shader set up
+    shader = std::make_unique<Shader>(vs.c_str(), fs.c_str());
+    //VBO & VAO
+    glGenBuffers(1, &VBO);
+    glGenVertexArrays(1, &VAO);
+    //Initialization
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), NULL);
+    glEnableVertexAttribArray(0);
+}
+Atom::Atom(const vec2& position, const vec2& velocity, float radius, float mass) : Atom()
+{
+    this->pos = position;
+    this->vel = velocity;
+    this->radius = radius / 20.0; //TODO:ADD A PROJECTION MATRIX SO I DON'T HAVE TO USE THIS HACK
+    this->mass = mass;
+}
+void Atom::draw()
+{
+    shader->use();
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_LINE_LOOP, 0, NUM_VERTICES);
+}
 
 void Atom::update(float dt) {
     this->pos += this->vel * dt;
--- cpp/src/Atom.h
+++ cpp/src/Atom.h
@@ -4,18 +4,37 @@
 
 #ifndef CPP_WASM_PHYSICS_ATOM_H
 #define CPP_WASM_PHYSICS_ATOM_H
-#include "Vec2.h"
+#if __EMSCRIPTEN__
+#include <emscripten/emscripten.h>
+#endif
+#include "vec2.h"
+#include "Shader.h"
+#include "vec2.h"
+#include <memory>
 
 class Atom {
-private:
-    Vec2 pos, vel;
-    float mass, radius;
-    Atom(Vec2 pos, Vec2 vel, float radius=1.0, float mass=1.0) :
-    pos(pos), vel(vel), radius(radius), mass(mass)
-    {
-    }
+public:
+    Atom();
+    Atom(const vec2& position, const vec2& velocity, float radius=1.0, float mass=1.0);
+
+    //vec2 force{vec2(0,0)};
+    vec2 vel{vec2(0, 0)};
+    vec2 pos{vec2(0.0, 0.0)};
+    double radius{1.0};
+    double mass{1.0};
+
+    void draw();
+    //void processInput();
+    //Updates physics of the Atom
     void update(float dt);
     /* TODO: integrate C++ and javascript */
+
+private:
+    std::unique_ptr<Shader> shader;
+    static const int NUM_VERTICES = 32;
+    float vertices[3*NUM_VERTICES];
+    unsigned int VAO=0;
+    unsigned int VBO=0;
 };
 
 
