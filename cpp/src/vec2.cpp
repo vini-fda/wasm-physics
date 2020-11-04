2f073a52f546d8cc34f8c659df1021c701b313f4
Vinicius Freitas de Almeida <vinifreitas.d.a@gmail.com>
2020-11-04 12:46:33 -0300

    Changed Vec2 to vec2

--- cpp/src/vec2.cpp
+++ cpp/src/vec2.cpp
@@ -2,20 +2,17 @@
 // Created by vinicius on 02/11/2020.
 //
 
-#include "Vec2.h"
+#include "vec2.h"
 #include <limits>
 #include <cmath>
 #include <cstring>
+#include <cstdint>
 
 int32_t ulpsDistance(float a, float b);
 bool nearlyEqual(float a, float b,
                  float fixedEpsilon, int ulpsEpsilon);
 
-Vec2::Vec2(float x, float y) {
-    this->x = x; this->y = y;
-}
-
-Vec2& Vec2::operator=(const Vec2 &rv) {
+vec2& vec2::operator=(const vec2 &rv) {
     if(this != &rv) {
         this->x = rv.x;
         this->y = rv.y;
@@ -23,52 +20,52 @@ Vec2& Vec2::operator=(const Vec2 &rv) {
     return *this;
 }
 
-Vec2 &Vec2::operator+=(const Vec2 &rv) {
+vec2 &vec2::operator+=(const vec2 &rv) {
     this->x += rv.x;
     this->y += rv.y;
     return *this;
 }
 
-Vec2 &Vec2::operator-=(const Vec2 &rv) {
+vec2 &vec2::operator-=(const vec2 &rv) {
     this->x -= rv.x;
     this->y -= rv.y;
     return *this;
 }
 
-Vec2 &Vec2::operator*=(const float &alpha) {
+vec2 &vec2::operator*=(const float &alpha) {
     this->x *= alpha;
     this->y *= alpha;
     return *this;
 }
 
-Vec2 &Vec2::operator/=(const float &alpha) {
+vec2 &vec2::operator/=(const float &alpha) {
     this->x /= alpha;
     this->y /= alpha;
     return *this;
 }
 
-Vec2 operator+(Vec2 lv, const Vec2 &rv) {
+vec2 operator+(vec2 lv, const vec2 &rv) {
     return (lv += rv);
 }
 
-Vec2 operator-(Vec2 lv, const Vec2 &rv) {
+vec2 operator-(vec2 lv, const vec2 &rv) {
     return (lv -= rv);
 }
 
 //Performs outer product in canonical basis
-float operator^(const Vec2& lv, const Vec2 &rv) {
+float operator^(const vec2& lv, const vec2 &rv) {
     return (lv.x * rv.y - lv.y * rv.x);
 }
 
-float operator*(const Vec2 &lv, const Vec2 &rv) {
+float operator*(const vec2 &lv, const vec2 &rv) {
     return lv.x*rv.x + lv.y*rv.y;
 }
 
-bool Vec2::operator==(const Vec2 &rv) const {
+bool vec2::operator==(const vec2 &rv) const {
     return (x == rv.x) && (y == rv.y);
 }
 
-bool near(const Vec2 &lv, const Vec2 &rv) {
+bool near(const vec2 &lv, const vec2 &rv) {
     /* Tests if two vectors are close to one another */
     /* TODO: EXPLAIN THEORY */
     /* TODO: IMPROVE THIS */
@@ -84,33 +81,33 @@ bool near(const Vec2 &lv, const Vec2 &rv) {
     }
 }
 
-float norm2(const Vec2 &v) {
+float norm2(const vec2 &v) {
     /* Square of the norm of v */
     return v*v;
 }
 
-Vec2 operator*(Vec2 lv, float alpha) {
+vec2 operator*(vec2 lv, float alpha) {
     return (lv *= alpha);
 }
 
-Vec2 operator*(float alpha, Vec2 rv) {
+vec2 operator*(float alpha, vec2 rv) {
     return (rv *= alpha);
 }
 
 /* Projects vector lv into vector rv's span */
-Vec2 proj(const Vec2& lv, const Vec2 &rv) {
+vec2 proj(const vec2& lv, const vec2 &rv) {
     return (lv*rv)/(rv*rv) * rv;
 }
 
-float norm(const Vec2 &v) {
+float norm(const vec2 &v) {
     return sqrtf(norm2(v));
 }
 
-float dist2(const Vec2 &lv, const Vec2 &rv) {
+float dist2(const vec2 &lv, const vec2 &rv) {
     return norm2(lv - rv);
 }
 
-float dist(const Vec2 &lv, const Vec2 &rv) {
+float dist(const vec2 &lv, const vec2 &rv) {
     return norm(lv - rv);
 }
 
---
+++ cpp/src/vec2.h
@@ -0,0 +1,51 @@
+//
+// Created by vinicius on 02/11/2020.
+//
+
+#ifndef CPP_WASM_PHYSICS_VEC2_H
+#define CPP_WASM_PHYSICS_VEC2_H
+
+
+class vec2 {
+public:
+    float x, y;
+public:
+    vec2() : x(0.0), y(0.0) {};
+    vec2(const vec2& v) {
+      x = v.x;
+      y = v.y;
+    };
+    vec2(float x, float y) : x(x), y(y) {};
+    //Assignment
+    vec2& operator=(vec2 const& rv);
+    //Arithmetic operators(compound assignment)
+    vec2& operator+=(const vec2& rv);
+    vec2& operator-=(const vec2& rv);
+    vec2& operator*=(const float& alpha);
+    vec2& operator/=(const float& alpha);
+
+    //Arithmetic operators(binary)
+    friend vec2 operator+(vec2 lv, const vec2& rv);
+    friend vec2 operator-(vec2 lv, const vec2& rv);
+    //Outer product ^
+    friend float operator^(const vec2& lv, const vec2& rv);
+    //Inner product %
+    friend float operator*(const vec2& lv, const vec2& rv);
+    //Comparison
+    bool operator == (const vec2& rv) const;
+    friend bool near(const vec2& lv, const vec2& rv);
+    //Norm & distance
+    friend float norm2(const vec2& v);
+    friend float norm(const vec2& v);
+    friend float dist2(const vec2& lv, const vec2& rv);
+    friend float dist(const vec2& lv, const vec2& rv);
+
+    //Mult by scalar(left and right)
+    friend vec2 operator*(vec2 lv, float alpha);
+    friend vec2 operator*(float alpha, vec2 rv);
+    //Projection
+    friend vec2 proj(const vec2& lv, const vec2& rv);
+};
+
+
+#endif //CPP_WASM_PHYSICS_VEC2_H
